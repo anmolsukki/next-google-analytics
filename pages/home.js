f76d5774b7ceeddd116c2dc1828ea91672de3515
anmolsukki <anmolsukki94@gmail.com>
2020-05-04 07:26:17 +0530

    first commit

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+ 
+import Layout from "../components/layout.js"
+ 
+export default class extends Component {
+  render () {
+    return (
+      <Layout>
+        <div>Homepage.</div>
+      </Layout>
+    )
+  }
+}
